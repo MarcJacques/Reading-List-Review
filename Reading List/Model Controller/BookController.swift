85cc22d2917044e38135ec831bc79daf4eea46a0
Marc <marc.daniel.jacques@gmail.com>
2020-11-25 10:40:09 -0500

    Setting up files

---
+++ Reading List/Model Controller/BookController.swift
@@ -0,0 +1,12 @@
+//
+//  BookController.swift
+//  Reading List
+//
+//  Created by Marc Jacques on 11/25/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+class BookController {
+    
+}
---
+++ Reading List/Model/book.swift
@@ -0,0 +1,14 @@
+//
+//  book.swift
+//  Reading List
+//
+//  Created by Marc Jacques on 11/25/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Book {
+    let title: String
+    var read: Bool
+}
